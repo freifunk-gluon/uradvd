ac41c29bfea3833c76575a5fe5b90278e7ee8a8c
aiyion <git@aiyionpri.me>
2025-09-05 22:44:45 +0200

    feat: Implement --adv-link-mtu
    an option taking one argument: the MTU to advertise on the link
    
    Must not be less than 1280 bytes.

--- uradvd.c
+++ uradvd.c
@@ -41,6 +41,9 @@
 #define MAX_PREFIXES 8
 #define MAX_RDNSS 3
 
+/* This is in bytes */
+#define AdvLinkMTU 0u
+
 /* These are in seconds */
 #define AdvValidLifetime 86400u
 #define AdvPreferredLifetime 14400u
@@ -66,6 +69,7 @@ enum {
 	OPT_PREFERRED_LIFETIME,
 	OPT_MAX_ROUTER_ADV_INTERVAL,
 	OPT_MIN_ROUTER_ADV_INTERVAL,
+	OPT_ADV_LINK_MTU,
 	OPT_VERSION,
 };
 
@@ -102,6 +106,8 @@ static struct global {
 
 	const char *ifname;
 
+	uint16_t adv_link_mtu;
+
 	uint32_t adv_valid_lifetime;
 	uint32_t adv_preferred_lifetime;
 	uint16_t adv_default_lifetime;
@@ -123,6 +129,7 @@ static struct global {
 	.adv_default_lifetime = AdvDefaultLifetime,
 	.max_rtr_adv_interval = MaxRtrAdvInterval,
 	.min_rtr_adv_interval = MinRtrAdvInterval,
+	.adv_link_mtu = AdvLinkMTU,
 };
 
 
@@ -515,6 +522,8 @@ static void send_advert(void) {
 	struct icmpv6_opt lladdr = {ND_OPT_SOURCE_LINKADDR, 1, {}};
 	memcpy(lladdr.data, G.iface.mac, sizeof(G.iface.mac));
 
+	struct nd_opt_mtu mtu = {ND_OPT_MTU, 1, 0, htonl(G.adv_link_mtu)};
+
 	struct nd_opt_prefix_info prefixes[G.n_prefixes];
 
 	size_t i;
@@ -553,6 +562,7 @@ static void send_advert(void) {
 		IOVEC(prefixes),
 		IOVEC_IF(G.n_rdnss > 0, rdnss),
 		IOVEC_IF(G.n_rdnss > 0, rdnss_ips),
+		IOVEC_IF(G.adv_link_mtu > 0, mtu),
 	};
 
 	struct sockaddr_in6 addr = {
@@ -600,7 +610,7 @@ static void usage(void) {
 			"[ --default-lifetime <seconds> ] [ --rdnss <ip> ... ]\n"
 			"[ --valid-lifetime <seconds> ] [ --preferred-lifetime <seconds> ]\n"
 			"[ --max-router-adv-interval <seconds> ] [ --min-router-adv-interval <seconds> ]\n"
-			"[ --version ]\n");
+			"[ --adv-link-mtu <bytes> ] [ --version ]\n");
 }
 
 static void version(void) {
@@ -668,6 +678,7 @@ static void parse_cmdline(int argc, char *argv[]) {
 		[OPT_PREFERRED_LIFETIME] = {"preferred-lifetime", required_argument, 0, 0},
 		[OPT_MAX_ROUTER_ADV_INTERVAL] = {"max-router-adv-interval", required_argument, 0, 0},
 		[OPT_MIN_ROUTER_ADV_INTERVAL] = {"min-router-adv-interval", required_argument, 0, 0},
+		[OPT_ADV_LINK_MTU] = {"adv-link-mtu", required_argument, 0, 0},
 		[OPT_VERSION] = {"version", 0, 0, 0},
 		{0, 0, 0, 0}
 	};
@@ -729,6 +740,16 @@ static void parse_cmdline(int argc, char *argv[]) {
 					exit_error("invalid minimal router advertisement interval\n", 0);
 
 				G.min_rtr_adv_interval = val;
+
+				break;
+
+			case OPT_ADV_LINK_MTU:
+				val = strtoul(optarg, &endptr, 0);
+
+				if (!*optarg || *endptr || val < 1280 || val > UINT16_MAX)
+					exit_error("invalid advertised link MTU\n", 0);
+
+				G.adv_link_mtu = val;
 
 				break;
 
